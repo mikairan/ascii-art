3829bea3aa2e314eeac22f603e877c0872a045a7
mikairan <62975758+mikairan@users.noreply.github.com>
2020-11-02 18:14:21 -0800

    Update high_gravity.asm

--- high_gravity.asm
+++ high_gravity.asm
@@ -14,16 +14,16 @@
 ;========================
 
 Main:
-LDA $13D4	  ;\  checks if the code
-BNE return  ; | should actually be
-LDA $71		  ; | running at this time
-BNE return  ;/ 
-REP #$20	  ;\ 
-LDA $96		  ; | increases mario's
-CLC			    ; | Y-position by 2
-ADC #$0002  ; | subpixels every
-STA $96		  ; | frame, moving him
-SEP #$20	  ; | downward constantly
-RTL			    ;/
+LDA $13D4	;\  checks if the code
+BNE return	; | should actually be
+LDA $71		; | running at this time
+BNE return	;/ 
+REP #$20	;\ 
+LDA $96		; | increases mario's
+CLC			; | Y-position by 2
+ADC #$0002	; | subpixels every
+STA $96		; | frame, moving him
+SEP #$20	; | downward constantly
+RTL			;/
 return:
 RTL	
