5dead3b97d6f7409c6e4c179ce7565bce6890a74
mikairan <62975758+mikairan@users.noreply.github.com>
2023-10-04 00:43:45 -0700

    Update high_gravity.asm

--- high_gravity.asm
+++ high_gravity.asm
@@ -1,17 +1,10 @@
-;========================
-;left-side barrier tower
-; pseudo high gravity
-; by mikairanutshack
-;========================
-; note: if you want the
-; 'gravity' to be low,
-; pushing mario upward,
-; like the second section
-;of left-side barrier tower,
-; please edit the line
-; that says ADC #$0002
-; to say ADC #$FFFE instead
-;========================
+;left-side barrier tower gravity
+;by mikairanutshack
+
+;INC #2 = extremely high gravity (default)
+;INC #1 = high gravity
+;DEC #1 = low gravity
+;DEC #2 = extremely low gravity
 
 Main:
 LDA $13D4|!addr
@@ -19,7 +12,7 @@ ORA $71
 BNE +
 REP #$20
 LDA $96
-INC #2
+INC #2		;this line is configurable
 STA $96
 SEP #$20
-RTL	
+RTL
